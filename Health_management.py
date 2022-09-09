96709eb525de34d88d044cf4b7a55fd04849a2cb
Astaraj <astaraj.behera@gmail.com>
2022-09-10 01:01:38 +0530

    some improvement imposed to Health_management project.

--- Health_management.py
+++ Health_management.py
@@ -1,24 +1,23 @@
 # Health Management System
-# 3 clients - Harry, Rohan and Hammad
-# Total 6 files
+# 3 clients 
 # write a function that when executed takes as input client name
 # One more function to retrieve exercise or food for any client
 
 def getdate():
     import datetime
     return datetime.datetime.now()
-
+#Take input from client
 def take(k):
     if k == 1:
         c = int(input("Enter 1 for excersise and 2 for food: "))
         if (c == 1):
             value = input("Type here:\n")
-            with open("harry_ex.txt", "a") as op:
+            with open("Lucky_ex.txt", "a") as op:
                 op.write(str(getdate()) + ": " + value + "\n")
             print("Successfully written")
         elif (c == 2):
             value = input("Type here\n")
-            with open("harry_food.txt", "a") as op:
+            with open("Lucky_food.txt", "a") as op:
                 op.write(str([str(getdate())]) + ": " + value + "\n")
             print("Successfully written")
     elif (k == 2):
@@ -50,16 +49,16 @@ def take(k):
     else:
         print("Please enter valid input.")
 
-
+#retrive input from client
 def retrieve(k):
     if k == 1:
         c = int(input("Enter 1 for excersise and 2 for food: "))
         if (c == 1):
-            with open("harry_ex.txt") as op:
+            with open("Lucky_ex.txt") as op:
                 for i in op:
                     print(i, end = "")
         elif (c == 2):
-            with open("harry_food.txt") as op:
+            with open("Lucky_food.txt") as op:
                 for i in op:
                     print(i, end = "")
     elif (k == 2):
@@ -89,10 +88,10 @@ print("Health Management System: ")
 a = int(input("Press 1 for log the value and 2 for retrieve: "))
 
 if a == 1:
-    b = int(input("Press 1 for Harry,\n2 for Debendra,\n3 for Asit: "))
+    b = int(input("Press 1 for Lucky,\n2 for Debendra,\n3 for Asit: "))
     take(b)
 elif a == 2:
-    b = int(input("Press 1 for Harry,\n2 for Debendra,\n3 for Asit: "))
+    b = int(input("Press 1 for Lucky,\n2 for Debendra,\n3 for Asit: "))
     retrieve(b)
 else:
     print("Please enter valid input.")
