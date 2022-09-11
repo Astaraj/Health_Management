16681a921d7f705297b4e7eb868fe78ea7a35829
Astaraj <astaraj.behera@gmail.com>
2022-09-11 11:03:36 +0530

    Some changes made in water_snake game

--- water_snake.py
+++ water_snake.py
@@ -7,13 +7,13 @@ lis=["s" , "w" , "g"]
 
 chance=10
 num_of_chance=0
-human_point=08
+human_point=0
 system_point=0
 
 print("\t\t\tSnake Water Gun Game")
 print('''RULE: \nSnake vs. Water: Snake drinks the water hence wins.
 \nWater vs. Gun: The gun will drown in water, hence a point for water
-\nGun vs. Snake: Gun will kill the snake and win''')
+\nGun vs. Snake: Gun will kill the snake and win \n''')
 print("Give command as \ns for Snake \nw for Water \ng for Gun:")
 
 # making the game in while
@@ -24,37 +24,37 @@ while num_of_chance<chance:
     if human == system:
         human_point+=1
         system_point+=1
-        print(f"Your choice is {human} and system choice is {system} \nso Tie")
+        print(f"\nYour choice is {human} and system choice is {system} \nso Tie")
 
     # if user enter s
     elif human == "s" and system == "w":
         human_point+=1
-        print(f"Your choice is {human} and system choice is {system} \nso Human gets one point.")
+        print(f"\nYour choice is {human} and system choice is {system} \nso Human gets one point.")
 
     elif human == "s" and system == "g":
         system_point+=1
-        print(f"Your choice is {human} and system choice is {system} \nso system gets one point.")
+        print(f"\nYour choice is {human} and system choice is {system} \nso system gets one point.")
 
     # if user enter w
     elif human == "w" and system == "s":
         system_point+=1
-        print(f"Your choice is {human} and system choice is {system} \nso system gets one point.")
+        print(f"\nYour choice is {human} and system choice is {system} \nso system gets one point.")
 
     elif human == "w" and system == "g":
         human_point+=1
-        print(f"Your choice is {human} and system choice is {system} \nso Human gets one point.")
+        print(f"\nYour choice is {human} and system choice is {system} \nso Human gets one point.")
 
     # if user enter g
     elif human == "g" and system == "s":
         human_point+=1
-        print(f"Your choice is {human} and system choice is {system} \nso Human gets one point.")
+        print(f"\nYour choice is {human} and system choice is {system} \nso Human gets one point.")
 
     elif human == "g" and system == "w":
         system_point+=1
-        print(f"Your choice is {human} and system choice is {system} \nso system gets one point.")
+        print(f"\nYour choice is {human} and system choice is {system} \nso system gets one point.")
 
     else:
-        print("Wrong input")
+        print("\nWrong input")
 
     num_of_chance += 1
     print(f"You left {chance - num_of_chance} out of {chance}")
@@ -63,10 +63,10 @@ while num_of_chance<chance:
 print("Game over")
 
 if human_point<system_point:
-    print(f"system win game led by {system_point} points")
+    print(f"\nsystem win game led by {system_point} points")
 
 elif human_point>system_point:
-    print(f"Human win game led by {human_point} points")
+    print(f"\nHuman win game led by {human_point} points")
 
 else:
-    print(f"Game DRAW both gets {human_point} points")
+    print(f"\nGame DRAW both gets {human_point} points")
